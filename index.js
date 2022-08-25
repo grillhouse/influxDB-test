617400066401b026916e8a4639174e48dd5fcefc
Grill House <Grill House>
2022-08-25 13:24:45 +0930

    node index add querry

--- index.js
+++ index.js
@@ -28,3 +28,22 @@ for (let i = 0; i < 5; i++) {
   }, 5000)
 }
 
+// Simple query
+
+let queryClient = client.getQueryApi(org)
+let fluxQuery = `from(bucket: "nodeBucket")
+ |> range(start: -10m)
+ |> filter(fn: (r) => r._measurement == "measurement1")`
+
+queryClient.queryRows(fluxQuery, {
+  next: (row, tableMeta) => {
+    const tableObject = tableMeta.toObject(row)
+    console.log(tableObject)
+  },
+  error: (error) => {
+    console.error('\nError', error)
+  },
+  complete: () => {
+    console.log('\nSuccess')
+  },
+})
