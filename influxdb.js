5a5303271cab667f7e3314496195256e40d7a3c8
Grill House <Grill House>
2022-08-25 14:57:25 +0930

    added nodejs influxDB query

--- influxdb.js
+++ influxdb.js
@@ -6,7 +6,7 @@ const url = 'https://europe-west1-1.gcp.cloud2.influxdata.com'
 const client = "nW9s_VqKP3hxVJRhfOr7Wnjz7f-7Kr2PCLWmZURmNIHK3AlB9ct-jAzVUBYAPKcdh31uGZcvFPejh8OC4McLLg=="
 
 // Write data
-
+let org = `robin.linuxos@gmail.com`
 
 function getData(dataName, range){
     
