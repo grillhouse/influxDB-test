ed58baf357a3fc2d628d68db09543008bbc889f4
Grill House <Grill House>
2022-08-25 13:26:29 +0930

    node index add querry

--- index.js
+++ index.js
@@ -47,3 +47,24 @@ queryClient.queryRows(fluxQuery, {
     console.log('\nSuccess')
   },
 })
+
+// aggregate querry 
+
+queryClient = client.getQueryApi(org)
+fluxQuery = `from(bucket: "nodeBucket")
+ |> range(start: -10m)
+ |> filter(fn: (r) => r._measurement == "measurement1")
+ |> mean()`
+
+queryClient.queryRows(fluxQuery, {
+  next: (row, tableMeta) => {
+    const tableObject = tableMeta.toObject(row)
+    console.log(tableObject)
+  },
+  error: (error) => {
+    console.error('\nError', error)
+  },
+  complete: () => {
+    console.log('\nSuccess')
+  },
+})
