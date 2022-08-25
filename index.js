7393dc91236dab9afd68628d3ebd78a6c7952739
Grill House <Grill House>
2022-08-25 13:22:07 +0930

    node index add added

---
+++ index.js
@@ -0,0 +1,30 @@
+repl.repl.ignoreUndefined=true
+
+const {InfluxDB, Point} = require('@influxdata/influxdb-client')
+
+const token = process.env.INFLUXDB_TOKEN_NODE
+const url = 'https://europe-west1-1.gcp.cloud2.influxdata.com'
+
+const client = new InfluxDB({url, token})
+
+// Write data
+
+let org = `robin.linuxos@gmail.com`
+let bucket = `nodeBucket`
+
+let writeClient = client.getWriteApi(org, bucket, 'ns')
+
+for (let i = 0; i < 5; i++) {
+  let point = new Point('measurement1')
+    .tag('tagname1', 'tagvalue1')
+    .intField('field1', i)
+
+  void setTimeout(() => {
+    writeClient.writePoint(point)
+  }, i * 1000) // separate points by 1 second
+
+  void setTimeout(() => {
+    writeClient.flush()
+  }, 5000)
+}
+
