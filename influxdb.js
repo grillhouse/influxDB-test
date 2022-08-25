a969e49940eb56f79c5f9b54ec936dc6778b544a
Grill House <Grill House>
2022-08-25 15:07:57 +0930

    added nodejs influxDB query

--- influxdb.js
+++ influxdb.js
@@ -1,9 +1,10 @@
 const {InfluxDB, Point} = require('@influxdata/influxdb-client')
 
-const token = process.env.INFLUXDB_TOKEN_NODE
 const url = 'https://europe-west1-1.gcp.cloud2.influxdata.com'
 
-const client = "nW9s_VqKP3hxVJRhfOr7Wnjz7f-7Kr2PCLWmZURmNIHK3AlB9ct-jAzVUBYAPKcdh31uGZcvFPejh8OC4McLLg=="
+const token = "nW9s_VqKP3hxVJRhfOr7Wnjz7f-7Kr2PCLWmZURmNIHK3AlB9ct-jAzVUBYAPKcdh31uGZcvFPejh8OC4McLLg=="
+
+const client = new InfluxDB({url, token})
 
 // Write data
 let org = `robin.linuxos@gmail.com`
