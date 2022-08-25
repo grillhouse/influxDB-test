aa6820f2770efa16ee2a8b48ec47aa109f78d31c
Grill House <Grill House>
2022-08-25 15:09:53 +0930

    ''

--- influxdb.js
+++ influxdb.js
@@ -13,8 +13,8 @@ let queryClient = client.getQueryApi(org)
 function getData(dataName, range){
 
     let fluxQuery = `from(bucket: "bbq-temps")
-    |> range(start: ${range})
-    |> filter(fn: (r) => r._measurement == ${dataName})`
+    |> range(start: ${str(range)})
+    |> filter(fn: (r) => r._measurement == ${str(dataName)})`
 
     queryClient.queryRows(fluxQuery, {
     next: (row, tableMeta) => {
