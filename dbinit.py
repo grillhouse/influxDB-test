bba53ea97f6d049a6279d6d3326ad811c485f49a
Grill House <Grill House>
2022-08-25 13:04:01 +0930

    init file added

--- dbinit.py
+++ dbinit.py
@@ -7,3 +7,18 @@ org = "robin.linuxos@gmail.com"
 url = "https://europe-west1-1.gcp.cloud2.influxdata.com"
 
 client = influxdb_client.InfluxDBClient(url=url, token=token, org=org)
+
+
+bucket="<BUCKET>"
+
+write_api = client.write_api(write_options=SYNCHRONOUS)
+   
+for value in range(5):
+  point = (
+    Point("measurement1")
+    .tag("tagname1", "tagvalue1")
+    .field("field1", value)
+  )
+  write_api.write(bucket=bucket, org="robin.linuxos@gmail.com", record=point)
+  time.sleep(1) # separate points by 1 second
+
